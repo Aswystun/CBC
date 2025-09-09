df50d042f04d64fdceeea0f56f4ced32cf1c8af4
Alex Swystun <122708137+Aswystun@users.noreply.github.com>
2025-09-09 16:49:11 -0500

    Refactor paths and enhance directory creation

--- Week4/03_Trimming.sh
+++ Week4/03_Trimming.sh
@@ -9,14 +9,14 @@ MyID=aubats001
 
 
 WD=/scratch/${MyID}/Felis_catus
-DD=/scratch/${MyID}/Felis_catus/RawData
-CD=/scratch/${MyID}/Felis_catus/CleanData
-PCQ=PostCleanQuality
+RAWDATA=/scratch/${MyID}/Felis_catus/RawData
+CLEAN=/scratch/${MyID}/Felis_catus/CleanData
+QC=PostCleanQuality
 ADAPTERS=/scratch/${MyID}/Felis_catus/Adapters
 
 
-mkdir ${CD}
-mkdir ${WD}/${PCQ}
+mkdir -p ${CD}
+mkdir -p ${PCQ}
 mkdir -p $ADAPTERS
 
 
@@ -32,29 +32,55 @@ cd ${DD}
 
 ### Make list of file names to Trim
 
-ls | grep ".fastq" |cut -d "_" -f 1 | sort | uniq > list
+
+for R1 in $RAWDATA/*_1.fastq*; do
+    # Skip if no files matched
+    [ -e "$R1" ] || { echo "No input FASTQ files found in $RAWDATA"; exit 1; }
+
+    # Infer R2 filename
+    R2=${R1/_1.fastq/_2.fastq}
+    base=$(basename "$R1" | sed 's/_1\.fastq.*//')
+
+    echo "Processing sample: $base"
+    echo "  R1: $R1"
+    echo "  R2: $R2"
+
+    trimmomatic PE -threads 8 -phred33 \
+        "$R1" "$R2" \
+        "$CLEAN/${base}_1_paired.fastq.gz" "$CLEAN/${base}_1_unpaired.fastq.gz" \
+        "$CLEAN/${base}_2_paired.fastq.gz" "$CLEAN/${base}_2_unpaired.fastq.gz" \
+        ILLUMINACLIP:$ADAPTERS:2:30:10 \
+        LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+    fastqc -t 8 -o "$QC" \
+        "$CLEAN/${base}_1_paired.fastq.gz" \
+        "$CLEAN/${base}_2_paired.fastq.gz"
+done
+
+# return that the script is completed
+echo "✅ Trimming + QC finished. Results in:"
+echo "   $CLEAN"
+echo "   $QC"
 
 
-# Need to relative path the adapters
-#cp /home/${MyID}/class_shared/AdaptersToTrim_All.fa .
-
-
-while read i
-do
-
-
-   java -jar /apps/x86-64/apps/spack_0.19.1/spack/opt/spack/linux-rocky8-zen3/gcc-11.3.0/trimmomatic-0.39-iu723m2xenra563gozbob6ansjnxmnfp/bin/trimmomatic-0.39.jar   \
-PE -threads 6 -phred33 \
-    "$i"_1.fastq "$i"_2.fastq  \
-    ${CD}/"$i"_1_paired.fastq ${CD}/"$i"_1_unpaired.fastq  ${CD}/"$i"_2_paired.fastq ${CD}/"$i"_2_unpaired.fastq \
-    ILLUMINACLIP:AdaptersToTrim_All.fa:2:35:10 HEADCROP:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:6:30 MINLEN:36
 
 
 
-fastqc ${CD}/"$i"_1_paired.fastq --outdir=${WD}/${PCQ}
-fastqc ${CD}/"$i"_2_paired.fastq --outdir=${WD}/${PCQ}
 
-done<list			# This is the end of the loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #########################  Now compress your results files from the Quality Assessment by FastQC 
 ## move to the directory with the cleaned data
