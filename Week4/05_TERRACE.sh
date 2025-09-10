a6b47a401350dc0bd5229b9424e815d22065741f
Alex Swystun <122708137+Aswystun@users.noreply.github.com>
2025-09-10 06:40:42 -0500

    Refactor 05_TERRACE.sh for module loading and variables
    Updated the script to load necessary modules and set variables for the analysis. Improved conda environment handling and adjusted terrace command with dynamic input and output paths.

--- Week4/05_TERRACE.sh
+++ Week4/05_TERRACE.sh
@@ -1,12 +1,33 @@
-#!/bin/bash
 
-cd /scratch/aubats001/Felis_catus
+source /apps/profiles/modules_asax.sh.dyn
+module load star/2.7.6a
+module load samtools/1.18
+module load miniforge3
 
+# Set Variables
+MyID=aubats001
+WD=/scratch/${MyID}/Felis_catus
+CLEAN=${WD}/CleanData
+REFERENCE=${WD}/Reference_Genome
+REFGENOME=${REFERENCE}/genome.fa
+STAR_ALIGN=${WD}/aligned
+BAM_OUTPUT=${STAR_ALIGN}/SRR3218716_Aligned.sortedByCoord.out.bam
+TERRACE_OUTPUT=${STAR_ALIGN}/SRR3218716_terrace_output.gtf
+FORWARD=${CLEAN}/SRR3218716_1_paired.fastq.gz
 
-module load miniforge3   # or anaconda3, if available
-eval "$(conda shell.bash hook)"  # may be needed on some clusters
-conda install mamba -n base -c conda-forge
-conda create -n terrace_env -c conda-forge -c bioconda terrace
+read_length=$(zcat ${FORWARD} | sed -n '2p' | wc -m)
+echo "Detected read length: ${read_length}"
+
+# Activate Conda and install environment
+eval "$(conda shell.bash hook)"
+if ! conda env list | grep -q terrace_env; then
+    conda install -y mamba -n base -c conda-forge
+    mamba create -y -n terrace_env -c conda-forge -c bioconda terrace
+fi
 conda activate terrace_env
 
-terrace -i input.bam -o output.gtf -fa reference.fa --read_length 150
+
+terrace -i ${BAM_OUTPUT} \
+        -o ${TERRACE_OUTPUT} \
+        -fa ${REFGENOME} \
+        --read_length ${read_length}
